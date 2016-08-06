9185d366fcc93e79dae0622112f7c34ab466d746
qiejinkai <qiejinkai@126.com>
2016-08-06 21:38:25 +0800

    使用ecache作为内存缓存； 新增ecache实现和测试类

---
+++ src/main/java/com/qjk/qblog/util/CacheManagerUtil.java
@@ -0,0 +1,87 @@
+package com.qjk.qblog.util;
+
+import javax.annotation.Resource;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class CacheManagerUtil {
+
+	private static final String CAHCE_SYSTEM = "systemConfig";
+	private static final String CACHE_LOGIN = "loginConfig";
+
+	@Resource
+	private CacheManager cacheManager;
+
+	private CacheManagerUtil() {
+
+	}
+
+	public Cache getCache() {
+
+		if (cacheManager != null) {
+
+			Cache cache = cacheManager.getCache("default");
+			return cache;
+		}
+		return null;
+	}
+
+	public Cache getCache(String cacheName) {
+		if (cacheManager != null) {
+			if (Value.isEmpty(cacheName)) {
+
+				Cache cache = cacheManager.getCache("default");
+				return cache;
+			}
+
+			Cache cache = cacheManager.getCache(cacheName);
+			return cache;
+		}
+
+		return null;
+
+	}
+
+	public Object getSystemCache(String key) {
+
+		Cache cache = getCache(CAHCE_SYSTEM);
+		if (cache != null) {
+			return cache.get(key).get();
+		}
+
+		return null;
+	}
+
+	public boolean putSystemCache(String key, Object obj) {
+		Cache cache = getCache(CAHCE_SYSTEM);
+		if (cache != null) {
+			cache.put(key, obj);
+			return true;
+		}
+
+		return false;
+	}
+
+	public Object getLoginCache(String key) {
+
+		Cache cache = getCache(CACHE_LOGIN);
+		if (cache != null) {
+			return cache.get(key).get();
+		}
+
+		return null;
+	}
+
+	public boolean putLoginCache(String key, Object obj) {
+		Cache cache = getCache(CACHE_LOGIN);
+		if (cache != null) {
+			cache.put(key, obj);
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ src/test/java/qblog/TestCacheManageUtil.java
@@ -0,0 +1,34 @@
+package qblog;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import com.qjk.qblog.data.User;
+import com.qjk.qblog.util.CacheManagerUtil;
+
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class TestCacheManageUtil extends AbstractJUnit4SpringContextTests {
+	
+	@Resource
+	CacheManagerUtil util;
+	@Test
+	public void test(){
+		String key = "user:id:1";
+		User user = new User();
+		user.setEmail("qiejinkai@qq.com");
+		user.setPhone("13051701098");
+		user.setNick("凯爷");
+		boolean result = util.putLoginCache("user:id:1", user);
+		System.out.println(result);
+		System.out.println( util.getLoginCache(key));
+		
+		User u1 = (User) util.getLoginCache(key);
+		System.out.println(" u1 : "+ u1.getEmail()+" , " +u1.getNick());
+		User u2 = (User) util.getSystemCache(key);
+		System.out.println(" u2 : "+ u2.getEmail()+" , " +u2.getNick());
+		
+	}
+}
